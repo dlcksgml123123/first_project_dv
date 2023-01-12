23e21e47a8dccc07082df9df70662e0ccb61e248
armycar <credit102@naver.com>
2023-01-12 20:42:23 +0900

    에러수정

--- src/main/java/com/greenart/yogio/lchwork/Controller/LoginController.java
+++ src/main/java/com/greenart/yogio/lchwork/Controller/LoginController.java
@@ -6,7 +6,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.greenart.yogio.member.repository.MemberInfoRepository;
 
 @RestController("/member")
 public class LoginController {
